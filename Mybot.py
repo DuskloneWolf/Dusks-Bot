ad04ae3267d02a3c0a53630809609e89add5dfd6
DuskloneWolf <romaargollo@gmail.com>
2025-05-19 16:03:45 -0600

    Update and rename Mybot.py to mybot.py

--- Mybot.py
+++
@@ -1,3 +0,0 @@
-# Dusks-Bot
-$ highrise mybot:Bot <https://high.rs/room?id=68271f368036e918b721fdf2&invite_id=682ba8b779bba8ddf8286d7b>
-<c1a94a086bb0ee1b32962cf1b8bfa9c7f4b56078788433e44536cf0662460ee2>
